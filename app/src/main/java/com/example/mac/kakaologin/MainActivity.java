8572bb1dbbaa6388c2408611f07088b68d739ebf
young minkim <mac@youngui-MacBook-Pro.local>
2017-04-05 00:05:53 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mac/kakaologin/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mac.kakaologin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
